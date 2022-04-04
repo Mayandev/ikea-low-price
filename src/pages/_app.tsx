9ed8dfbbe1daa099bd29d72df2753d9f67aa474b
mayandev <phillzou@gmail.com>
2022-04-04 17:31:14 +0800

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import * as ga from '@/utils/ga';
+
 import "@/styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on(`routeChangeComplete`, handleRouteChange);
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off(`routeChangeComplete`, handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
