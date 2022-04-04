b1e1b35b310257fa5bfe8fecbed1dfa63be95d6d
mayandev <phillzou@gmail.com>
2022-04-04 15:47:34 +0800

    feat: add dark mode

--- src/pages/components/Banner/index.tsx
+++ src/pages/components/Banner/index.tsx
@@ -1,26 +1,22 @@
-import { CategoryMap } from "@/const";
-import { ProductContext } from "@/pages";
-import { RoomType } from "@/types";
-import Image from "next/image";
-import { useContext } from "react";
+import { CategoryMap } from '@/const';
+import { ProductContext } from '@/pages';
+import { RoomType } from '@/types';
+import Image from 'next/image';
+import { useContext } from 'react';
 
 export default function Banner() {
   const { category } = useContext(ProductContext);
   return (
-    <div className="h-72 sm:h-52  w-full bg-[#b76d32] bg-cover relative bg-[url('/hero-bg.png')] lg:bg-[url('/hero-bg-lg.png')]">
-      <div className="sm:hidden absolute bottom-0 left-0 h-24 w-full bg-gradient-to-b from-[#b76d32]/0 to-[#f2f2f2]" />
+    <div className="h-72 sm:h-52  w-full bg-ikea-orange dark:bg-dark-black dark:sm:bg-dark-black sm:bg-ikea-blue bg-cover relative bg-[url('/hero-bg.png')] lg:bg-[url('/hero-bg-lg.png')]">
+      <div className="sm:hidden absolute bottom-0 left-0 h-24 w-full bg-gradient-to-b from-ikea-orange/0 to-[#f2f2f2] dark:to-dark-black" />
       <div className="h-40 relative text-white flex flex-col items-center justify-end ">
         <div className="flex">
           <Image width={14} height={30} src="/icons/award-left.svg" />
           <span className="font-bold text-2xl mx-3">宜家特价榜</span>
           <Image width={14} height={30} src="/icons/award-right.svg" />
         </div>
-        <div className="font-bold text-3xl mt-1">
-          {CategoryMap[category as RoomType]}
-        </div>
-        <div className="text-white text-xs mt-8">
-          数据来自宜家官网 | 每周更新
-        </div>
+        <div className="font-bold text-3xl mt-1">{CategoryMap[category as RoomType]}</div>
+        <div className="text-white text-xs mt-8">数据来自宜家官网 | 每周更新</div>
       </div>
     </div>
   );
--- src/pages/components/Logo/index.tsx
+++ src/pages/components/Logo/index.tsx
@@ -2,10 +2,10 @@ export default function Logo() {
   return (
     <div className="sm:px-16 xl:px-48 2xl:px-64 py-10 hidden sm:block">
       <div>
-        <span className="bg-[#FBD914] w-20 h-20 rounded-full inline-block" />
-        <span className="bg-[#1A52BF] w-20 h-20 ml-4 inline-block" />
+        <span className="bg-ikea-yellow dark:bg-dark-black dark:border-8 dark:border-ikea-yellow w-20 h-20 rounded-full inline-block" />
+        <span className="bg-ikea-blue dark:bg-dark-black dark:border-8 dark:border-ikea-blue w-20 h-20 ml-4 inline-block" />
       </div>
-      <h1 className="font-logo font-bold text-6xl inline-block tracking-wide px-2">
+      <h1 className="font-logo dark:text-white font-bold text-6xl inline-block tracking-wide px-2">
         IKEA
       </h1>
     </div>
